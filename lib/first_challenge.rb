bb2bad12e9e6a334385856b417c415165427144e
David Sim <wsim903@gmail.com>
2018-09-29 21:02:54 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,9 @@ def first_challenge
     }
   }
 
-  #your code here
-
+  contacts["Freddy Mercury"].values[2].map do |each|
+    each.delete_if("strawberry")
+  contacts  
 
   #remember to return your newly altered contacts hash!
   contacts
